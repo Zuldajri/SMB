c24a84dfacd2af4f59236754b2c1e39ed5e6a18f
Zuldajri <w.matyas15@hotmail.fr>
2022-05-05 15:41:00 +0200

    Update PSConfigMainVM.ps1

--- script/PSConfigMainVM.ps1
+++ script/PSConfigMainVM.ps1
@@ -19,12 +19,14 @@ if ($size -lt $maxSize){
 
 
 if ($PrimaryUseDataDisk -eq 'True'){
-    #Initialize Data Disks
-    Get-Disk | ` 
-    Where partitionstyle -eq 'raw' | ` 
-    Initialize-Disk -PartitionStyle MBR -PassThru | ` 
-    New-Partition -AssignDriveLetter -UseMaximumSize | ` 
-    Format-Volume -FileSystem NTFS -NewFileSystemLabel "datadisk" -Confirm:$false
+    if ($DomainMethod -eq 'extraVm'){
+        #Initialize Data Disks
+        Get-Disk | ` 
+        Where partitionstyle -eq 'raw' | ` 
+        Initialize-Disk -PartitionStyle MBR -PassThru | ` 
+        New-Partition -AssignDriveLetter -UseMaximumSize | ` 
+        Format-Volume -FileSystem NTFS -NewFileSystemLabel "datadisk" -Confirm:$false
+    }
 }
 
 #Enable Time Zone Redirection
